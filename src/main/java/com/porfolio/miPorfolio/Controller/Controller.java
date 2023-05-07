5d973bf4a63bad4e008fbd1b9b0663c148f6a6ad
Pedro Juarez <pedrojuarezvisuara@gmail.com>
2023-05-07 20:39:11 -0300

    modificando controler

--- src/main/java/com/porfolio/miPorfolio/Controller/Controller.java
+++ src/main/java/com/porfolio/miPorfolio/Controller/Controller.java
@@ -12,7 +12,7 @@ import com.porfolio.miPorfolio.service.IPersonaService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+/*import org.springframework.web.bind.annotation.CrossOrigin;*/
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+/*@CrossOrigin(origins = "aqui va el local hots 4200 si lo necesitara")*/
 public class Controller {
     
     
